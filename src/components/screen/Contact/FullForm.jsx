f0b303f6a6d66cc1f960f697daa6e4915cb307d3
SymuNadeem <msymunnadeem@gmail.com>
2024-07-16 12:45:21 +0600

    contact color changes in dark

--- src/components/screen/Contact/FullForm.jsx
+++ src/components/screen/Contact/FullForm.jsx
@@ -27,7 +27,7 @@ const FullForm = () => {
   };
 
   return (
-    <div className="mt-10 px-2 sm:px-11.5 py-15 bg-grayBg rounded-[20px]">
+    <div className="mt-10 px-2 sm:px-11.5 py-15 bg-white dark:bg-grayBg shadow-lg rounded-[20px]">
       <form onSubmit={handleSubmit} className="space-y-10">
         <FormInput
           type="text"
@@ -55,7 +55,7 @@ const FullForm = () => {
         
           <button
             type="submit"
-            className="w-[133px] h-[50px] border-[2px] border-border_Sub hover:border-white bg-transparent button_sub text-white rounded-xl"
+            className="w-[133px] h-[50px] border-[2px] border-border_Sub hover:border-white bg-transparent button_sub text-black dark:text-white rounded-xl"
           >
             Submit
           </button>
--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -4,7 +4,7 @@ import TitleText from "../components/shared/TitleText";
 const Blogs = () => {
   return (
     <section>
-      <div className="mt-10 px-5 py-5 sm:px-10 sm:py-13 bg-black rounded-[20px]">
+      <div className="mt-10 px-5 py-5 sm:px-10 sm:py-13 bg-white dark:bg-black rounded-[20px]">
         <TitleText title="My Blogs" />
         
       </div>
