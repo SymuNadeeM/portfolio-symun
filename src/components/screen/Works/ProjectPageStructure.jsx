7b9081490de792f32bd03403c06e9419ab7c6254
SymuNadeem <msymunnadeem@gmail.com>
2024-07-18 21:13:29 +0600

    dark color

--- src/components/screen/Works/ProjectPageStructure.jsx
+++ src/components/screen/Works/ProjectPageStructure.jsx
@@ -8,7 +8,7 @@ const ProjectPageStructure = ({ ProjectData, halfLength }) => {
         {ProjectData.slice(0, halfLength).map((items) => (
           <div
             key={items.id}
-            className="p-5 bg-white shadow-lg dark:bg-grayBg dark:border border-borderRe rounded-xl w-full h-fit"
+            className="p-3 bg-white shadow-lg dark:bg-grayBg dark:border border-borderRe rounded-xl w-full h-fit"
           >
             <Link to={"#"} className="rounded-xl mb-4 block overflow-hidden">
               <div className="w-full h-[200px]">
@@ -20,7 +20,7 @@ const ProjectPageStructure = ({ ProjectData, halfLength }) => {
             </Link>
             <p className="text-xs capitalize text-black dark:text-grayText">{items.title}</p>
             <Link to={"#"}>
-              <h5 className="font-medium text-black dark:text-white hover:text-activeColor duration-300 leading-6 capitalize">
+              <h5 className="font-medium text-black dark:text-white hover:text-activeColor dark:hover:text-activeColor duration-300 leading-6 capitalize">
                 {items.name}
               </h5>
             </Link>
@@ -31,7 +31,7 @@ const ProjectPageStructure = ({ ProjectData, halfLength }) => {
         {ProjectData.slice(halfLength).map((items) => (
           <div
             key={items.id}
-            className="p-5 bg-white shadow-lg dark:bg-grayBg dark:border border-borderRe rounded-xl w-full h-fit"
+            className="p-3 bg-white shadow-lg dark:bg-grayBg dark:border border-borderRe rounded-xl w-full h-fit"
           >
             <Link to={"#"} className="rounded-xl mb-4 block overflow-hidden">
               <div className="w-full h-[200px]">
@@ -43,7 +43,7 @@ const ProjectPageStructure = ({ ProjectData, halfLength }) => {
             </Link>
             <span className="text-xs capitalize text-black dark:text-grayText">{items.title}</span>
             <Link to={"#"}>
-              <h5 className="font-medium text-black dark:text-white hover:text-activeColor duration-300 leading-6 capitalize">
+              <h5 className="font-medium text-black dark:text-white hover:text-activeColor duration-300 leading-6 capitalize dark:hover:text-activeColor">
                 {items.name}
               </h5>
             </Link>
