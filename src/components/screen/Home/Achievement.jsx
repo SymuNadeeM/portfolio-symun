0d4a05bcbd33f4a51237ee5bfe7926dceb104637
SymuNadeem <msymunnadeem@gmail.com>
2024-07-27 23:17:45 +0600

    static blog page

--- src/components/screen/Home/Achievement.jsx
+++ src/components/screen/Home/Achievement.jsx
@@ -7,7 +7,7 @@ const Achievement = () => {
       {aboutData.achievements.map((achievement, index) => (
         <div
           key={index}
-          className="bg-[#508C9B] dark:bg-grayBackground rounded-md p-4 flex flex-col justify-center items-center shadow-lg"
+          className="bg-[#315f6b] hover:bg-gradient-to-r hover:from-[#99def0] dark:bg-grayBackground rounded-md p-4 flex flex-col justify-center items-center shadow-lg"
         >
           <h2 className="text-3xl font-semibold text-white dark:text-grayText">{achievement.value}</h2>
           <p className="pt-2 text-sm uppercase text-center text-white dark:text-grayText">
