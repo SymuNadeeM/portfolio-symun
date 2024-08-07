e353aee20af9a27e6ba47dff7b9895be2af9e5e1
SymuNadeem <msymunnadeem@gmail.com>
2024-08-07 13:11:06 +0600

    count added

--- src/components/screen/Home/Achievement.jsx
+++ src/components/screen/Home/Achievement.jsx
@@ -1,20 +1,33 @@
-import { aboutData } from "../../../data/aboutPage";
-
+import CountUp from "react-countup";
+// import { aboutData } from "../../../data/aboutPage";
 
 const Achievement = () => {
   return (
     <div className="mt-10 sm:grid sm:grid-cols-3  gap-3 space-y-4 sm:space-y-0">
-      {aboutData.achievements.map((achievement, index) => (
-        <div
-          key={index}
-          className="bg-[#315f6b] dark:bg-grayBackground  rounded-md p-4 flex flex-col justify-center items-center shadow-lg"
-        >
-          <h2 className="text-3xl font-semibold text-white dark:text-grayText">{achievement.value}</h2>
-          <p className="pt-2 text-sm uppercase text-center text-white dark:text-grayText">
-            {achievement.label}
-          </p>
-        </div>
-      ))}
+      <div className="bg-[#315f6b] dark:bg-grayBackground  rounded-md p-4 flex flex-col justify-center items-center shadow-lg">
+        <h2 className="text-3xl font-semibold text-white dark:text-grayText">
+          <CountUp end={3} duration={1} />+
+        </h2>
+        <p className="pt-2 text-sm uppercase text-center text-white dark:text-grayText">
+          Years of Experience
+        </p>
+      </div>
+      <div className="bg-[#315f6b] dark:bg-grayBackground  rounded-md p-4 flex flex-col justify-center items-center shadow-lg">
+        <h2 className="text-3xl font-semibold text-white dark:text-grayText">
+          <CountUp end={30} duration={5} />+
+        </h2>
+        <p className="pt-2 text-sm uppercase text-center text-white dark:text-grayText">
+          PROJECTS COMPLETED
+        </p>
+      </div>
+      <div className="bg-[#315f6b] dark:bg-grayBackground  rounded-md p-4 flex flex-col justify-center items-center shadow-lg">
+        <h2 className="text-3xl font-semibold text-white dark:text-grayText">
+          <CountUp end={10} duration={3} />+
+        </h2>
+        <p className="pt-2 text-sm uppercase text-center text-white dark:text-grayText">
+          HAPPY CLIENTS
+        </p>
+      </div>
     </div>
   );
 };
